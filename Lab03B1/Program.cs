0f2ac74d35bd9e8894a54c12dc32e724527901ab
hvu26510 <hvu26510@gmail.com>
2023-05-20 18:19:49 +0700

    push

---
+++ Lab03B1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03B1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("So phan tu cua mang:");
+            int sopt = int.Parse(Console.ReadLine());
+
+            int[] songuyen = new int[sopt];
+            //Nhập phần tử
+            for (int i = 0; i < songuyen.Length; i++)
+            {
+                Console.WriteLine($"Nhap phan tu so: {i + 1}");
+                songuyen[i] = int.Parse(Console.ReadLine());
+            }
+
+            //tăng dần
+            Array.Sort(songuyen);
+            Console.WriteLine($"Sap xep tang dan");
+            foreach (int pt in songuyen)
+            {
+                Console.Write(pt + "  ");
+            }
+
+            //giảm dần(đảo ngược của tăng dần)
+            Array.Reverse(songuyen);
+            Console.WriteLine();
+            Console.WriteLine($"Sap xep giam dan");
+            foreach (int pt in songuyen)
+            {
+                Console.Write(pt + "  ");
+            }
+
+            // Xuat min
+            Console.WriteLine("Min = " + songuyen[songuyen.Length - 1]);
+
+            //Tính TBC
+            int tong = 0, count = 0;
+            foreach (int pt in songuyen)
+            {
+                if (pt % 3 == 0)
+                {
+                    tong = tong + pt;
+                    count++;
+                }
+            }
+            Console.WriteLine($"TBC = {tong*1.0/count}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab03B1C2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Lab03B1C2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList songuyen = new ArrayList();
+            int count = 1;
+            while (true)
+            {
+                Console.WriteLine("Nhap phan tu thu " + count);
+
+                Console.WriteLine("Nhap 'q' de dung nhap ");
+                string inPut = Console.ReadLine();
+                if (inPut == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    songuyen.Add(int.Parse(inPut));
+                }
+                count++;
+            }
+            // Sap xep
+            songuyen.Sort();
+
+            foreach (int i in songuyen)
+            {
+                Console.Write(i + " ");
+            }
+
+            songuyen.Reverse();
+
+            Console.WriteLine();
+            foreach (int i in songuyen)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
